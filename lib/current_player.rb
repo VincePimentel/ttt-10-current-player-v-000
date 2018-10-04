419545b89931424cb9aeeb7c53ca87cdb5ab9c79
Vince Andrew Pimentel <contact@vincep.me>
2018-10-04 05:31:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,3 @@
 def turn_count(board)
-  count = 0
-  loop do
-    if board[count] == "X" | board[count] == "O"
-      step = 0
-      board.each do |turns|
-        step += 1
-      end
-      return step
-    end
-  end
+
 end
