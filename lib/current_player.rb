426fc1a4dbabf0598f17493702dfb4732c45a7f6
Vince Andrew Pimentel <contact@vincep.me>
2018-10-04 05:48:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |turns|
-    if turns.strip == "X" | turns.strip == "O"
+    if turns.strip == "X" || turns.strip == "O"
       counter += 1
     end
     return counter
